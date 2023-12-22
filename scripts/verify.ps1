f5a70cc378cfc20cdfc40ccdef976d4efdf3a7bb
Enyium <123484196+Enyium@users.noreply.github.com>
2023-12-22 16:38:52 +0100

    Improved scripts.

--- scripts/verify.ps1
+++ scripts/verify.ps1
@@ -1,8 +1,14 @@
+# The crate for each `windows` crate version must be able to be successfully built with the minimal and the full feature set.
+cargo build --features windows_v0_48
+if (-not $?) { throw 'Failure' }
 cargo build --features windows_v0_48_all
 if (-not $?) { throw 'Failure' }
 
+cargo build --features windows_v0_52
+if (-not $?) { throw 'Failure' }
 cargo build --features windows_v0_52_all
 if (-not $?) { throw 'Failure' }
 
+# Test (same feature as on test modules).
 cargo test --features windows_latest_compatible_all
 if (-not $?) { throw 'Failure' }
