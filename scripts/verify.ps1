985c76d9fe29951b911a83f339b3f437a3df414c
Enyium <123484196+Enyium@users.noreply.github.com>
2023-12-22 23:38:52 +0100

    Added helpful version output to verify script.

--- scripts/verify.ps1
+++ scripts/verify.ps1
@@ -1,14 +1,17 @@
 # The crate for each `windows` crate version must be able to be successfully built with the minimal and the full feature set.
+Write-Host 'Building for `windows` v0.48...'
 cargo build --features windows_v0_48
 if (-not $?) { throw 'Failure' }
 cargo build --features windows_v0_48_all
 if (-not $?) { throw 'Failure' }
 
+Write-Host 'Building for `windows` v0.52...'
 cargo build --features windows_v0_52
 if (-not $?) { throw 'Failure' }
 cargo build --features windows_v0_52_all
 if (-not $?) { throw 'Failure' }
 
 # Test (same feature as on test modules).
+Write-Host 'Testing...'
 cargo test --features windows_latest_compatible_all
 if (-not $?) { throw 'Failure' }
