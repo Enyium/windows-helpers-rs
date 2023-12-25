7ad47c85ee41f2b1b646239464016731c8525a7c
Enyium <123484196+Enyium@users.noreply.github.com>
2023-12-25 22:52:52 +0100

    (Added output to publish.ps1 on abort.)

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,7 +1,10 @@
 # Note: You can use `cargo publish` flags like `--dry-run` on this script.
 
 $answer = Read-Host "Really publish now? (y/N)"
-if ($answer -cne 'y') { exit }
+if ($answer -cne 'y') {
+    Write-Host 'Aborting.'
+    exit
+}
 
 & "$PSScriptRoot\verify.ps1"
 if (-not $?) { throw 'Failure' }
